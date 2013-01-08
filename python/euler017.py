cda6e857305717fcb87d40ed93246165b89c1717
Nolan Knill <nolanknill@rogers.com>
2013-01-08 11:52:52 -0500

    Add get letters function

--- python/euler017.py
+++ python/euler017.py
@@ -27,8 +27,21 @@ def get_teens(number):
 	return length
 
 def get_tens(number):
-	length = len(tens[number])
+	number_string = str(number)
+	if number >= 10 and number < 20:
+		length = get_teens(int(number_string[1]))
+	else:
+		tens_digit = int(number_string[0])
+		length = len(tens[tens_digit])
+		ones_digit = int(number_string[1])
+		length += get_ones(ones_digit)
+
 	return length
+
+def get_hundreds(number):
+	number_string = str(number)
+	letters = get_ones(int(number_string[0])) + len("hundred") #get hundreds length of letters
+	letters += get_tens(int(number_string[1:2]))
 
 def get_letters(number):
 	number_string = str(number)
@@ -38,13 +51,24 @@ def get_letters(number):
 		letters = get_ones(number)
 
 	if length == 2:
-		if number >10 and number < 10
+		letters = get_tens(number)
+
+	if length == 3:
+		hundreds_digit = int(number_string[0])
+		letters = get_ones(hundreds_digit)
+
+	if length == 4:
+		letters = len("onethousand")
+		
 	return letters
 
+for i in range(1,1001):
+	print(i, get_tens(i))
+"""
 for i in range(0,10):
 	print(get_letters(i))
 
 total_letters = 0
-
+"""
 #for i in range (1,1001):
 	#total_letters += get_letters(i)
